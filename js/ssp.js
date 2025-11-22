dcd193bc3cd1b04c34c224335267a547727e733c
Safro-maker <dofy.mingo@bk.ru>
2025-11-22 17:08:40 +0300

    Модернизация проекта

--- js/ssp.js
+++ js/ssp.js
@@ -1,49 +1,80 @@
+
+const compm = document.getElementById("compm");
+compm.textContent = "Computer motion:"
+const buttons = document.getElementById("btn");
+    const stbtn = document.createElement("button");
+        stbtn.textContent = "Stone";
+        stbtn.classList.add("btn");
+    const scbtn = document.createElement("button");
+        scbtn.textContent = "Scissors";
+        scbtn.classList.add("btn");
+    const ppbtn = document.createElement("button");
+        ppbtn.textContent = "Paper";
+        ppbtn.classList.add("btn");
+buttons.appendChild(stbtn);
+buttons.appendChild(scbtn);
+buttons.appendChild(ppbtn);
+const comp = document.getElementById("comp");
+    const phrase1 = document.createElement("span");
+        phrase1.textContent = "Computer:"
+        comp.appendChild(phrase1);
+    let score1 = document.createElement("span");
+        score1.textContent = " 0";
+        comp.appendChild(score1);
+const player = document.getElementById("hum");
+    const phrase2 = document.createElement("span");
+        phrase2.textContent = "Player:"
+        player.appendChild(phrase2);
+    let score2 = document.createElement("span");
+        score2.textContent = " 0";
+        player.appendChild(score2);
+const win = document.getElementById("win");
+win.textContent = "" 
+let humanScore = 0;
+let computerScore = 0;
 function computerMotion(max) {
     let y=Math.floor(Math.random()*max)
+    console.log(y);
     if (y==0) return "stone";
     if(y==1) return "scissors";
     if(y==2) return "paper";      //Функция для рандомного выбора компьютером
 }
-function humanMotion() {
-    let h=prompt("stone or scissors or paper?"," ");
-    h=h.toLowerCase();
-    if (h=="stone") return "stone";
-    if (h=="scissors") return "scissors";
-    if (h=="paper") return "paper";   //Функция для выбора игрока
-}
-function playRound() {
-    const hs=humanMotion();
+function playRound(hs) {
     const cs=computerMotion(3);
-    if(hs==cs) {alert("Draw, bro!");return "draw"};
-    if(hs=="stone"&&cs=="scissors") {alert("YOOO, take it easy, you win!");
-        return "hs"};
-    if(hs=="scissors"&&cs=="paper") {alert("YOOO, take it easy, you win!");
-        return "hs"};
-    if(hs=="paper"&&cs=="stone") {alert("YOOO, take it easy, you win!");
-        return "hs"};
-    if(cs=="stone"&&hs=="scissors") {alert("HAHAHA, computer more intelligent!");
-        return "cs"};
-    if(cs=="scissors"&&hs=="paper") {alert("HAHAHA, computer more intelligent!");
-        return "cs"};
-    if(cs=="paper"&&hs=="stone") {alert("HAHAHA, computer more intelligent!");
-        return "cs"};
+    compm.textContent = "Computer motion:" + " " + cs;
+    if(hs==cs) {return "draw"};
+    if(hs=="stone"&&cs=="scissors") {return "hs"};
+    if(hs=="scissors"&&cs=="paper") {return "hs"};
+    if(hs=="paper"&&cs=="stone") {return "hs"};
+    if(cs=="stone"&&hs=="scissors") {return "cs"};
+    if(cs=="scissors"&&hs=="paper") {return "cs"};
+    if(cs=="paper"&&hs=="stone") {return "cs"};
 }
-function playGame() {
-    let humanScore=0;
-    let computerScore=0;
-    while(humanScore<3&&computerScore<3) {          //Игра не закончится пока кто-то не наберёт 3 очка
-        let result = playRound();
-        if(result=="hs") humanScore+=1;
-        if(result=="cs") computerScore+=1;
-    };                                              
-    if(humanScore>computerScore) alert("YOU WIN!");
-    if(computerScore>humanScore) alert("YOU LOSE!");
+function winl(humScore,compScore) {
+    if (humScore>compScore) win.textContent = "You win!";
+    else win.textContent = "You lose!";
 }
-
-function start() {
-    let p = prompt("START? Yes or No?"," ");
-    p=p.toLowerCase();
-    if (p=="yes") {playGame();start()}
-    else start();
+function playGame(hs) {        //Игра не закончится пока кто-то не наберёт 3 очка
+    const result = playRound(hs);
+    if(result=="hs") {humanScore+=1;score2.textContent=`${humanScore}`};
+    if(result=="cs") {computerScore+=1;score1.textContent=`${computerScore}`};
+    if (humanScore>=3||computerScore>=3) winl(humanScore,computerScore);
 }
-start()
+function handChoice(value) {
+    return value.toLowerCase();
+}
+stbtn.addEventListener("click", () => playGame(stbtn.textContent.toLowerCase()))
+scbtn.addEventListener("click", () => playGame(scbtn.textContent.toLowerCase()))
+ppbtn.addEventListener("click", () => playGame(ppbtn.textContent.toLowerCase()))
+const div = document.getElementById("reset");
+const reset = document.createElement("button");
+reset.textContent = "Reset";
+reset.addEventListener("click", () => {
+    win.textContent = "";
+    humanScore=0;
+    computerScore=0;
+    score1.textContent = " 0";
+    score2.textContent = " 0";
+    compm.textContent = "Computer motion:"
+})
+div.appendChild(reset);
